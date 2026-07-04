ac3914d786aa6aed600c632c0caad9f3c6542f9f
Ranganath Gururaj Acharya <ranganath.acharya@subex.com>
2026-07-04 17:58:55 +0530

    Initial project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# Mobile Money Fraud Simulator
+
+Production-grade synthetic mobile money fraud data generator.
---
+++ src/main.py
@@ -0,0 +1,5 @@
+def main():
+    print('Mobile Money Fraud Simulator - Sprint 1')
+
+if __name__=='__main__':
+    main()
